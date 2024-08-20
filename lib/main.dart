041fecac0b3a1265450e6435caa05a609389f8ff
dawnfire05 <dawnfire05@naver.com>
2024-08-20 18:09:37 +0900

    feat: receive messages using/from fcm

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,44 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:happit_flutter/routes/app_routes.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+
+//백그라운드 알림 수신
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp(); // 여기에 initializeApp을 다시 해줄 필요가 있나?
+
+  print("Handling a background message: ${message.messageId}");
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print(fcmToken);
+
+  // await FirebaseMessaging messaging = FirebaseMessaging.instance.setAutoInitEnabled(true); fcm Token 관련 설정
+
+  // 포그라운드 알림 수신
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print(
+          'Message also contained a notification: ${message.notification?.title}, ${message.notification?.body}');
+    }
+  });
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   runApp(const MyApp());
 }
 
