7d382ece2645ec768c477bca8180b9033d81b7d0
dawnfire05 <dawnfire05@naver.com>
2024-11-30 11:27:48 +0900

    feat: check authorization

---
+++ lib/app/modules/user/presentation/bloc/user_bloc.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:happit_flutter/app/modules/user/data/repository/user_repository.dart';
+import 'package:injectable/injectable.dart';
+
+part 'user_bloc.freezed.dart';
+
+@injectable
+class UserBloc extends Bloc<UserEvent, UserState> {
+  final UserRepository _repository;
+  UserBloc(this._repository) : super(const _Initial()) {
+    on<_Load>((event, emit) async {
+      emit(const _Loading());
+
+      try {
+        final profile = await _repository.getProfile();
+      } on Exception catch (e) {
+        emit(const _Unauthenticated());
+      }
+    });
+    on<_getProfile>((event, emit) {});
+  }
+}
+
+@freezed
+abstract class UserEvent with _$UserEvent {
+  const factory UserEvent.load() = _Load;
+  const factory UserEvent.getProfile() = _getProfile;
+}
+
+@freezed
+abstract class UserState with _$UserState {
+  const UserState._();
+  const factory UserState.initial() = _Initial;
+  const factory UserState.loading() = _Loading;
+  const factory UserState.unauthenticated() = _Unauthenticated;
+  const factory UserState.authenticated() = _Authenticated;
+
+  bool get isAuthenticated => this is _Authenticated;
+}
