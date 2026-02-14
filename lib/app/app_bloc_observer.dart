49dd7640ac6f7baccb710c57a2cc8c8773430144
dawnfire05 <dawnfire05@naver.com>
2026-02-15 00:19:51 +0900

    chore: bloc 상태 변화 디버그프린트

--- lib/app/app_bloc_observer.dart
+++ lib/app/app_bloc_observer.dart
@@ -1,11 +1,10 @@
-import 'dart:developer';
-
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppBlocObserver extends BlocObserver {
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    log('${bloc.runtimeType} $transition');
+    debugPrint('${bloc.runtimeType} $transition');
   }
 }
